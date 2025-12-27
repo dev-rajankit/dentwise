0333afa7c851b953923a86e010e83e2410ad50ef
Ankit Raj <ankitrajkumar1546024@gmail.com>
2025-12-27 21:12:48 +0530

    landing page completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs'
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +17,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "DentWise - AI Powered Dental Assistant",
-  description: "Get instant dental advice through voice calls with our AI assistant. Available 24/7.",
+  description:
+    "Get instant dental advice through voice calls with our AI assistant. Avaiable 24/7.",
 };
 
 export default function RootLayout({
@@ -32,15 +27,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    
+      <ClerkProvider
+        appearance={{
+          variables: {
+            colorPrimary: "#e78a53",
+            colorBackground: "#f3f4f6",
+            colorText: "#111827",
+            colorTextSecondary: "#6b7280",
+            colorInputBackground: "#f3f4f6",
+          },
+        }}
       >
-        {children}
-      </body>
-    </html>
-    </ClerkProvider>
+        <html lang="en">
+          <body className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}>
+            {/* this is done in the home page component */}
+            {/* <UserSync /> */}
+            <Toaster />
+            {children}
+          </body>
+        </html>
+      </ClerkProvider>
     
   );
 }
